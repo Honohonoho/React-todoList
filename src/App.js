e53429e81b1260b38d8f40061e848f1876e975eb
Honohonoho <751718620@qq.com>
2017-05-22 16:07:47 +0800

    修复 点击删除后checked会转移至下一项，而不是删除

--- src/App.js
+++ src/App.js
@@ -4,49 +4,41 @@ import './reset.css';
 import './App.css';
 import TodoInput from './TodoInput';
 import TodoItem from './TodoItem';
-import * as localStore from './localStore';
+// import * as localStore from './localStore';
 
 class App extends Component {
   constructor(props){
     super(props)
     this.state = {
       newTodo: '',
-      todoList: localStore.load('todoList') || []
+      todoList: [
+      ]
     }
   }
-
-  render() {
-    let willdo = this.state.todoList
+  render(){
+    let todos = this.state.todoList
       .filter((item)=> !item.deleted)
       .map((item,index)=>{
-      return ( //多个节点时需要加括号,<li>的数量取决于todoList
+      return (
         <li key={index}>
-          <TodoItem 
-            todo={item} 
-            onToggle={this.toggle.bind(this)}
-            onDelete={this.delete.bind(this)}/>
+          <TodoItem todo={item} onToggle={this.toggle.bind(this)}
+            onDelete={this.delete.bind(this)} />
         </li>
       )
     })
-
     return (
       <div className="App">
         <h1>我的待办</h1>
-        <div className="inputArea">
-          <TodoInput 
-            content={this.state.newTodo}
+        <div className="inputWrapper">
+          <TodoInput content={this.state.newTodo}
             onChange={this.changeTitle.bind(this)}
-            onSubmit={this.addTodo.bind(this)}/>
+            onSubmit={this.addTodo.bind(this)} />
         </div>
         <ol className="todoList">
-          {willdo}
+          {todos}
         </ol>
       </div>
     )
-  }
-  
-  componentDidUpdate(){  // componentDidUpdate 会在组件更新之后调用
-    localStore.save('todoList',this.state.todoList)
   }
   toggle(e,todo){
     todo.status = todo.status === 'completed' ? '' : 'completed'
@@ -60,7 +52,7 @@ class App extends Component {
   }
   addTodo(event){
     this.state.todoList.push({
-      id: idMarker(),
+      id: idMaker(),
       title: event.target.value,
       status: null,
       deleted: false
@@ -79,7 +71,7 @@ class App extends Component {
 export default App;
 
 let id = 0;
-function idMarker() {
-  id += 1;
-  return id;
+function idMaker(){
+  id +=1;
+  return id
 }
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,14 +1,14 @@
 import React,{ Component } from 'react';
 import './TodoItem.css';
 
-export default class TodoItem extends Component{
+export default class TodoItem extends Component {
 	render(){
 		return (
 			<div className="TodoItem">
-			<input type="checkbox" chencked={this.props.todo.status === 'completed'} 
-				onChange={this.toggle.bind(this)} /> 
-			<span className="title">{this.props.todo.title}</span>
-			<button onClick={this.delete.bind(this)}>删除</button>
+				<input type="checkbox" checked={this.props.todo.status === 'completed'}
+					onChange={this.toggle.bind(this)} />
+				<span className="title">{this.props.todo.title}</span>
+				<button onClick={this.delete.bind(this)}>删除</button>
 			</div>
 		)
 	}
